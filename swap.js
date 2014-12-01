76276c92c9c3532e240c5032afa0f089077a772d
Chad Wintzer <cwintzer@gmail.com>
2014-11-30 20:40:57 -0500

    initial commit

---
+++ swap.js
@@ -0,0 +1,20 @@
+﻿var images = [
+    "http://i.imgur.com/G2UnzhJ.jpg",
+    "http://i.imgur.com/itbpdYa.jpg",
+    "http://i.imgur.com/LCmC1Fv.jpg"
+];
+
+window.onload = replaceImages;
+
+function replaceImages() {
+    for (var i in document.images) {
+        var targetImage = document.images[i];
+        var randomImage = images[Math.floor(Math.random() * images.length)];
+        var height = targetImage.height;
+        var width = targetImage.width;
+
+        targetImage.src = randomImage;
+        targetImage.height = height;
+        targetImage.width = width;
+    }
+}
